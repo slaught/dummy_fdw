0d2c56aa80262c50496b0cf8dc9db0bbc95cc071
Chad Slaughter <chad.slaughter@gmail.com>
2014-05-27 17:02:34 -0500

    rearrange library load and tests.

--- dummy_data.sql
+++ dummy_data.sql
@@ -7,32 +7,3 @@ LANGUAGE C STRICT;
 CREATE FOREIGN DATA WRAPPER dummy_data
 HANDLER dummy_handler;
 
-create server amazon_s3 foreign data wrapper dummy_data
-options (
-  wrapper 's3fdw.s3fdw.S3Fdw'
-);
-
-
-CREATE USER MAPPING FOR public SERVER amazon_s3
-;
-
-CREATE FOREIGN TABLE stats(
-  dvalue date,
-  email varchar,
-  hour interval,
-  login_time interval,
-  logout_time interval,
-  available_time interval
-) SERVER amazon_s3
-OPTIONS (
-  hostname 's3.amazonaws.com',
-  bucketname 'reports',
-  filename 'hourly.csv',
-  format 'csv',
-  delimiter E',',
-  quote E'"',
-  header 'true'
-);
-
-select * from stats;
-
